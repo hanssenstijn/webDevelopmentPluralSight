a9e52394054b5b7f445f2179a0cb19807417d350
Hanssen <35262249+hanssenstijn@users.noreply.github.com>
2020-02-20 13:53:26 +0100

    Add files via upload
    save all the city places, including the states

---
+++ scriptCopy.js
@@ -0,0 +1,17 @@
+$('.btn').click(function() {
+    $('.text').text('loading . . .');
+
+    $.ajax({
+        type: "GET",
+        url: "https://api.meetup.com/2/cities",
+        success: function(data) {
+            $('.text').html('');
+            for (var i = 0; i < data.results.length; i++) {
+                var place = data.results[i].city + ", " + data.results[i].state
+                $('.text').append('<p>' + place + '</p>');
+            }
+
+        },
+        dataType: 'jsonp',
+    });
+});
